401fbd80aa0660a4c0555d4a30def0c4c6f9ce1b
davedwight <dpdwight@gmail.com>
2021-07-23 14:05:56 -0600

    stub out tests

--- api/server.test.js
+++ api/server.test.js
@@ -18,28 +18,40 @@ afterAll(async () => {
   await db.destroy();
 });
 
-describe("jokes router", () => {
-  describe("[GET] /api/jokes", () => {
-    test("gets all jokes from the jokes table", async () => {
-      const res = await request(server).get("/api/jokes");
-      expect(res.body).toMatchObject([
-        {
-          id: "0189hNRf2g",
-          joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later.",
-        },
-        {
-          id: "08EQZ8EQukb",
-          joke: "Did you hear about the guy whose whole left side was cut off? He's all right now.",
-        },
-        {
-          id: "08xHQCdx5Ed",
-          joke: "Why didn’t the skeleton cross the road? Because he had no guts.",
-        },
-      ]);
-    });
-    test("snapshot test", async () => {
-      const res = await request(server).get("/api/jokes");
-      expect(res.body).toMatchSnapshot();
-    });
+describe("[GET] /api/jokes", () => {
+  test("requests with a valid token get all jokes from the jokes table", async () => {
+    // make sure to check token validation
+    const res = await request(server).get("/api/jokes");
+    expect(res.body).toMatchObject([
+      {
+        id: "0189hNRf2g",
+        joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later.",
+      },
+      {
+        id: "08EQZ8EQukb",
+        joke: "Did you hear about the guy whose whole left side was cut off? He's all right now.",
+      },
+      {
+        id: "08xHQCdx5Ed",
+        joke: "Why didn’t the skeleton cross the road? Because he had no guts.",
+      },
+    ]);
   });
+  test("requests without a token are bounced with status 401", () => {});
+  test("requests with an invalid token are bounced with status 401", () => {});
+  test("snapshot test", async () => {
+    const res = await request(server).get("/api/jokes");
+    expect(res.body).toMatchSnapshot();
+  });
+});
+
+describe("[POST] /api/auth/register", () => {
+  test("registers a new user in the database", () => {});
+  test("register returns the new user information", () => {});
+  test("does not create new user in the database if username and password are not provided", () => {});
+  test("does not create new user if username is a duplicate", () => {});
+});
+describe("[POST] /api/auth/register", () => {
+  test("responds with status code 200 on valid credentials", () => {});
+  test("responds with a correctly formatted token", () => {});
 });
